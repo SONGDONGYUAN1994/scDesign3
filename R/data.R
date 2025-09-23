acedb655b5947c133cd465038726ca6df3f607ec
Dongyuan Song <dongyuansong@ucla.edu>
2025-09-22 18:14:01 -0700

    Add example data for vignittee

--- R/data.R
+++ R/data.R
@@ -3,3 +3,15 @@
 #' @return The corresponding SingleCellExperiment object
 #' @usage data("example_sce")
 "example_sce"
+
+#' A sparse matrix with example data
+#' @format A dgCMatrix with 100 rows (genes) and 2087 cols (cells)
+#' @return The corresponding sparse matrix object 'example_count'
+#' @usage data("example_count")
+"example_count"
+
+#' A numeric vector of pseudotime values
+#' @format A numeric vector with 2087 values
+#' @return The corresponding pseudotime of the 'example_count'
+#' @usage data("pseudotime")
+"pseudotime"
